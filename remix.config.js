139b65823d8007eff65f714db48c10f36331795a
luis1gonzalez <luis1gonzalez@hotmail.com>
2023-02-04 19:13:52 +0100

    resolviendo deploy

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: "vercel",
-  serverBuildTarget: "netlify",
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   future: {},
